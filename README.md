5b373bd52b76f8c2fcbe59ec04ce7e39e9d18fc4
Santana Fernandez <santanamcf01@gmail.com>
2022-09-27 15:08:09 -0700

    setup

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# pwa-jate
+# PWA-Jate
+
+  ## Description
+  This project is just a simple text editor.
+  
+  
+  ## Table of Contents
+  * [usage](#usage)
+  * [contributions](#contributions)
+  * [testing](#testing)
+  * [license](#license)
+  * [git](#git)
+  * [email](#email)
+  
+  
+  ## Usage
+  You use this project by answering the questions the program asks you when you run it.
+  ## License
+  MIT License
+  ## Contributions
+  Robertson Rojas and Josh Lee.
+  ## Tests
+
+    Run the following command: npm test
+  
+  
+  
+  ## Questions
+  If you have any questions about the repo, open an issue or contact me directly at santanamcf01@gmail.com. You can find more of my work at Santanamcf
--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,8 +14,21 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => console.error('putDb not implemented');
-
+console.log('Seeking DB data to update.')
+	const jateDB = await openDB('jate', 1)
+	const jateReadWrite = jateDB.transaction('jate', 'readwrite')
+	const jateObject = jateReadWrite.objectStore('jate')
+	const request = jateObject.put({ id: 1, value: DBContent })
+	const response = await request
+	console.log(`Did it work? ${response}`)
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+	console.log('Seeking DB data to update.')
+	const jateDB = await openDB('jate', 1)
+	const jateRead = jateDB.transaction('jate', 'readonly')
+	const jateObject = jateRead.objectStore('jate')
+	const request = jateObject.getAll(1)
+	const result = await request
+}
 
 initdb();
